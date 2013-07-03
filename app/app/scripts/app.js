c0ba2fd637b42ebfe4c539759ae357687cf707b9
Mark Seaman <mark.seaman@shrinking-world.com>
2013-07-03 09:41:34 -0600

    Auto commit Documents

--- app/app/scripts/app.js
+++ app/app/scripts/app.js
@@ -1,5 +1,16 @@
 'use strict';
 
+var files      = require('./files');
+
+//var files      = require('../files');
+
+// var read_topics = function (filename)
+// {
+//     files.read (filename, function (text) { 
+//         topics = [  { title:'home', text:text } ];
+//     });
+// }
+
 angular.module('angularHammerApp', [])
   .config(function ($routeProvider) {
     $routeProvider
--- app/app/scripts/controllers/main.js
+++ app/app/scripts/controllers/main.js
@@ -4,6 +4,7 @@ var topics =
     [ { title:'home', text:'Home is where the heart is' },
       { title:'help', text:'Help has arrived at last'   } ];
 
+
 //var page = 'home';
 
 angular.module('angularHammerApp')
@@ -12,11 +13,22 @@ angular.module('angularHammerApp')
 
         $scope.topics = topics;
 
+        $scope.editing = false;
+
+        $scope.title = '';
+
+        $scope.clear = function(){
+            $scope.page  = '';
+            $scope.title = '';
+            $scope.text  = '';
+        }
+        
         $scope.add = function(){
             $scope.topics.push({ title:$scope.title, text:$scope.text })
+            $scope.editing = false;
         }
 
-        $scope.select_topic = function(topic) {
+        $scope.select_topic = function(topic,autoclear=false) {
             for (var o in $scope.topics) {
                 if ($scope.topics[o].title===topic) {
                     $scope.page  = $scope.topics[o].title;
@@ -25,32 +37,14 @@ angular.module('angularHammerApp')
                     return;
                 }
             }
+            if (autoclear) { clear() }
         }
        
         $scope.navigate = function() {
-            $scope.select_topic($scope.page);
+            $scope.select_topic($scope.page, false);
         }
-
-        $scope.show = function() {
-            for (var o in $scope.topics) {
-                if ($scope.topics[o].title===$scope.page) {
-                    $scope.page  = $scope.topics[o].title;
-                    $scope.title = $scope.topics[o].title;
-                    $scope.text  = $scope.topics[o].text;
-                    return;
-                }
-            }
-            $scope.page  = '';
-            $scope.title = '';
-            $scope.text  = '';
-        }
-
-        $scope.title = '';
-
-        $scope.links = [
-            {text:'Root Page', href:'x'},
-            {text:'Other Page', href:'page'}
-        ];
 
         $scope.page = 'home';
+        $scope.navigate();
+
     })
